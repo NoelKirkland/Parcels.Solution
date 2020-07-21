03b4db425f42bd2fe6b4e9731c756a30b65a735b
Noel Kirkland <noelkirkland@gmail.com>
2020-07-21 14:18:19 -0700

    create basic file tree, implement boilerplate stuff, and create a new class called Parcel
    Co-authored-by: Erich Richter <erich.richter@gmail.com>

---
+++ Parcels/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Parcels.Models;
+
+namespace Parcels.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+
+      [HttpGet("/parcel/new")]
+      public ActionResult CreateForm()
+      {
+        return View();
+      }
+
+      [HttpPost("/parcel")]
+      public ActionResult Create(int height, int width, int length, int weight)
+      {
+        Parcel newParcel = new Parcel(height, width, length, weight);
+        return RedirectToAction("ItemDetails");
+      }
+    }
+}
---
+++ Parcels/Models/Parcel.cs
@@ -0,0 +1,44 @@
+namespace Parcels.Models
+{
+  public class Parcel
+  {
+    public int Height {get; set; }
+
+    public int Width {get; set; }
+
+    public int Length {get; set; }
+
+    public int Weight {get; set; }
+  
+    public Parcel(int height, int width, int length, int weight)
+    {
+      Height = height;
+      Width = width;
+      Length = length;
+      Weight = weight;
+    }
+  
+    public int Volume()
+    {   int volume = 0;
+        volume = Length * Width * Weight;
+
+        return volume;
+    }
+  
+    public int CostToShip(int volume)
+    {
+      int cost = 0;
+
+      if (volume <= 5)
+      {
+        cost += 5;
+        return cost;
+      }
+      else
+      {
+        return cost;
+      }
+    }
+  
+  }
+}
